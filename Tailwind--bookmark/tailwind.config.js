7a669110ee51eea870a71925b607d44b66f9841f
Magda Sz <pesahson@gmail.com>
2021-08-31 18:28:19 +0200

    nav/hero/feature section/footer done

--- Tailwind--bookmark/tailwind.config.js
+++ Tailwind--bookmark/tailwind.config.js
@@ -2,14 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bk-red": "hsl(0, 94%, 66%)",
+        "bk-blue": "hsl(231, 69%, 60%)",
+        "bk-dark-blue": "hsl(229, 31%, 21%)",
+        "bk-grayish-blue": "hsl(229, 8%, 60%)",
+      },
+    },
+    fontFamily: {
+      Rubik: ["Rubik, sans-serif"],
+    },
     container: {
       center: true,
-      padding: "1rem"
-    }
+      padding: "1rem",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
